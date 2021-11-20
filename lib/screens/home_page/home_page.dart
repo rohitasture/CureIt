94a120cb582a77c65316f085d21dc02b7edde11d
Rohit Asture <rohitasture0312@gmail.com>
2021-11-20 23:05:57 +0530

    Added doctor info page

--- lib/screens/home_page/home_page.dart
+++ lib/screens/home_page/home_page.dart
@@ -4,6 +4,7 @@ import 'package:cureit/screens/home_page/components/categories.dart';
 import 'package:cureit/screens/home_page/components/explore.dart';
 import 'package:cureit/screens/home_page/components/health_issues.dart';
 import 'package:cureit/screens/home_page/docinfo.dart';
+import 'package:cureit/screens/home_page/docpage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cureit/components/custom_bottom_nav_bar.dart';
@@ -109,62 +110,67 @@ class HomeScreen extends StatelessWidget {
               SizedBox(
                 height: height * 0.05,
               ),
-              Container(
-                height: 200,
-                child: Stack(
-                  children: [
-                    Positioned(
-                        top: 0,
-                        left: 0.05 * width,
-                        child: Material(
-                          child: Container(
-                            height: 180,
-                            width: width * 0.9,
-                            decoration: BoxDecoration(
-                              color: Colors.lightBlue[100],
-                              borderRadius: BorderRadius.circular(10),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, DoctorDetailPage.routeName);
+                },
+                child: Container(
+                  height: 200,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                          top: 0,
+                          left: 0.05 * width,
+                          child: Material(
+                            child: Container(
+                              height: 180,
+                              width: width * 0.9,
+                              decoration: BoxDecoration(
+                                color: Colors.lightBlue[100],
+                                borderRadius: BorderRadius.circular(10),
+                              ),
                             ),
-                          ),
-                        )),
-                    Positioned(
-                        top: 10,
-                        left: 0.05 * width,
-                        child: Card(
-                          elevation: 10.0,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
+                          )),
+                      Positioned(
+                          top: 10,
+                          left: 0.05 * width,
+                          child: Card(
+                            elevation: 10.0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Container(
+                              height: 150,
+                              width: 0.4 * width,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(
+                                  // fit: BoxFit.fill,
+                                  image: AssetImage("assets/images/doctor.jpg"),
+                                ),
+                              ),
+                            ),
+                          )),
+                      Positioned(
+                          top: 20,
+                          left: 0.5 * width,
                           child: Container(
                             height: 150,
-                            width: 0.4 * width,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              image: DecorationImage(
-                                // fit: BoxFit.fill,
-                                image: AssetImage("assets/images/doctor.jpg"),
-                              ),
+                            width: 0.45 * width,
+                            child: Column(
+                              children: [
+                                Text("Neeraj Chopra",
+                                    style: TextStyle(fontSize: 20)),
+                                Divider(
+                                  color: Colors.white,
+                                ),
+                                Text(
+                                    "Qualifications: MBBS, MD(surgery) AIIMS Rishikesh\nExperience: 2 years\nLocation : Jaipur",
+                                    style: TextStyle(fontSize: 15)),
+                              ],
                             ),
-                          ),
-                        )),
-                    Positioned(
-                        top: 20,
-                        left: 0.5 * width,
-                        child: Container(
-                          height: 150,
-                          width: 0.45 * width,
-                          child: Column(
-                            children: [
-                              Text("Neeraj Chopra",
-                                  style: TextStyle(fontSize: 20)),
-                              Divider(
-                                color: Colors.white,
-                              ),
-                              Text(
-                                  "Qualifications: MBBS, MD(surgery) AIIMS Rishikesh\nExperience: 2 years\nLocation : Jaipur",
-                                  style: TextStyle(fontSize: 15)),
-                            ],
-                          ),
-                        ))
-                  ],
+                          ))
+                    ],
+                  ),
                 ),
               ),
               SizedBox(
